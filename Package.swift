6d4cb92f11ff1c2581eb5a3c503d2072da05fda2
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-03 20:55:30 +0200

    Add ArgumentParser dependency to MakeSpriteSheet

--- Package.swift
+++ Package.swift
@@ -17,14 +17,20 @@ let package = Package(
             targets: ["MakeSpriteSheet"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SpriteSheetEngine"
         ),
-        .target(
-            name: "MakeSpriteSheet"
+        .executableTarget(
+            name: "MakeSpriteSheet",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
         ),
         .testTarget(
             name: "SpriteSheetEngineTests",
