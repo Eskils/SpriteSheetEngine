08a9af0c0fdf163464c39699b095dbc8c726e55b
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-04 08:25:07 +0200

    Add model operation settings and group under union ModelOperation

---
+++ Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/ModelOperation.swift
@@ -0,0 +1,26 @@
+//
+//  ModelOperation.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 04/06/2025.
+//
+
+import simd
+import CoreGraphics
+
+enum ModelOperation {
+    case transform(Transform)
+    case material(Material)
+}
+
+extension ModelOperation {
+    struct Transform: ModelApplicable {
+        var nodeID: String
+        var matrix = simd_float4x4(diagonal: .one)
+    }
+    
+    struct Material: ModelApplicable {
+        var nodeID: String
+        var color: CGColor
+    }
+}
