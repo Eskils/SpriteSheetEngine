005e187b59ffbb31c90f3d9865e58a8425a3b9e9
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-04 08:50:07 +0200

    Add union SpriteSheetDescription and underlying type Model

---
+++ Sources/SpriteSheetEngine/Types/SpriteSheetDescription+Model.swift
@@ -0,0 +1,15 @@
+//
+//  SpriteSheetDescription+Model.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 04/06/2025.
+//
+
+extension SpriteSheetDescription {
+    struct Model: SpriteSheetDescribable {
+        var model: ModelKind
+        var camera: CameraSettings
+        var operations: [SpriteSheetAxis.Kind: [ModelOperation]]
+        var axes: Set<SpriteSheetAxis>
+    }
+}
