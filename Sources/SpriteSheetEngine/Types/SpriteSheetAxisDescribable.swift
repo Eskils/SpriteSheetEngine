9d6e04ca594b1c14b65e0adb80c9b728eda5719e
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-04 08:45:42 +0200

    Add protocol SpriteSheetAxisDescribable

---
+++ Sources/SpriteSheetEngine/Types/SpriteSheetAxisDescribable.swift
@@ -0,0 +1,11 @@
+//
+//  SpriteSheetAxisDescribable.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 04/06/2025.
+//
+
+
+protocol SpriteSheetAxisDescribable {
+    var count: Int { get }
+}
