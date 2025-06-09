e88fdffd5c0f435fb496a07e667da91e2de1cd67
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-09 23:40:19 +0200

    Implement ModelOperation.MaterialDTO

---
+++ Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/Codable/ModelOperation/ModelOperation+MaterialDTO.swift
@@ -0,0 +1,34 @@
+//
+//  ModelOperation+MaterialDTO.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 09/06/2025.
+//
+
+extension ModelOperation {
+    struct MaterialDTO: ModelOperationDTO {
+        var type: ModelOperation.Kind
+        var nodeID: String
+        var color: ColorDTO
+    }
+}
+
+extension ModelOperation.MaterialDTO: Codable {
+}
+
+extension ModelOperation.MaterialDTO: DataTransferObject {
+    func toModel() -> ModelOperation.Material {
+        ModelOperation.Material(
+            nodeID: nodeID,
+            color: color.toModel()
+        )
+    }
+    
+    init(model: ModelOperation.Material) {
+        self.init(
+            type: .material,
+            nodeID: model.nodeID,
+            color: ColorDTO(model: model.color)
+        )
+    }
+}
