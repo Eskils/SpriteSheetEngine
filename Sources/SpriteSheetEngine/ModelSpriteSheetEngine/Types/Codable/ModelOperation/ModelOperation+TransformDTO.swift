b250d7345b10e61fe84f75c878855b2a682108d3
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-09 22:03:08 +0200

    Implement ModelOperation.TransformDTO

---
+++ Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/Codable/ModelOperation/ModelOperation+TransformDTO.swift
@@ -0,0 +1,34 @@
+//
+//  ModelOperation+TransformDTO.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 09/06/2025.
+//
+
+extension ModelOperation {
+    struct TransformDTO: ModelOperationDTO {
+        var type: ModelOperation.Kind
+        var nodeID: String
+        var matrix: Matrix4x4DTO
+    }
+}
+
+extension ModelOperation.TransformDTO: Codable {
+}
+
+extension ModelOperation.TransformDTO: DataTransferObject {
+    func toModel() -> ModelOperation.Transform {
+        ModelOperation.Transform(
+            nodeID: nodeID,
+            matrix: matrix.toModel()
+        )
+    }
+    
+    init(model: ModelOperation.Transform) {
+        self.init(
+            type: .transform,
+            nodeID: model.nodeID,
+            matrix: Matrix4x4DTO(model: model.matrix)
+        )
+    }
+}
