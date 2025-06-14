2ad236af3e59163c6121922c74d2bdf06e16d881
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-14 14:13:43 +0200

    Add enum SpriteSheetDescription.TransferableType

---
+++ Sources/SpriteSheetEngine/Types/SpriteSheetDescription/SpriteSheetDescription+TransferableType.swift
@@ -0,0 +1,13 @@
+//
+//  SpriteSheetDescription+TransferableType.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 14/06/2025.
+//
+
+
+extension SpriteSheetDescription {
+    enum TransferableType {
+        case json
+    }
+}
