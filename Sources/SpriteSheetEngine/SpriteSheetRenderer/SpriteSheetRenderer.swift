41da1b8e3647f8ecd493e73600f612d9fdf2f8a9
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-14 15:10:47 +0200

    Add interface SpriteSheetRenderer

---
+++ Sources/SpriteSheetEngine/SpriteSheetRenderer/SpriteSheetRenderer.swift
@@ -0,0 +1,15 @@
+//
+//  SpriteSheetRenderer.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 14/06/2025.
+//
+
+import CoreGraphics
+
+protocol SpriteSheetRenderer {
+    associatedtype Description: SpriteSheetDescribable
+    
+    func setup(description: Description) async throws
+    func makeImage(for operation: Description.Operation) async throws -> CGImage
+}
