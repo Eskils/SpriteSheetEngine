4b51e41c5979b25ca8347d2d208e195eda03cc0e
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-14 15:11:11 +0200

    Implement initial SpriteSheetEngine

--- Sources/SpriteSheetEngine/SpriteSheetEngine.swift
+++ Sources/SpriteSheetEngine/SpriteSheetEngine.swift
@@ -1,2 +1,27 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import CoreGraphics
+
+struct SpriteSheetEngine<Renderer: SpriteSheetRenderer> {
+    let renderer: Renderer
+    let description: Renderer.Description
+    
+    func spriteSheet() async throws -> CGImage {
+        try await renderer.setup(description: description)
+        
+        let imageTiler = try ImageTiler(
+            tileSize: description.export.size,
+            numberOfColumns: description.numberOfColumns,
+            numberOfImages: description.operations.count
+        )
+        
+        for operation in description.operations {
+            let tile = try await renderer.makeImage(for: operation)
+            try imageTiler.checkedAppend(tile: tile)
+        }
+        
+        return try imageTiler.checkedFinish()
+    }
+    
+    func export(to url: URL) async throws {
+    }
+}
