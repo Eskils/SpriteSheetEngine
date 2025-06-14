6042821097e6ac5caf965c0952a9247b62f67157
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-14 15:10:57 +0200

    Implement ModelSpriteSheetRenderer

---
+++ Sources/SpriteSheetEngine/SpriteSheetRenderer/ModelSpriteSheetRenderer.swift
@@ -0,0 +1,32 @@
+//
+//  ModelSpriteSheetRenderer.swift
+//  SpriteSheetEngine
+//
+//  Created by Eskil Gjerde Sviggum on 14/06/2025.
+//
+
+import CoreGraphics
+
+struct ModelSpriteSheetRenderer: SpriteSheetRenderer {
+    let renderer: any ModelRenderer
+    
+    @MainActor
+    init(description: SpriteSheetDescription.Model) {
+        switch description.model {
+        case .realityKit(let entity):
+            let renderer = RealityKitModelRenderer(size: description.export.size)
+            renderer.add(model: entity)
+            self.renderer = renderer
+        case .sceneKit:
+            fatalError("Not implemented")
+        }
+    }
+    
+    func setup(description: SpriteSheetDescription.Model) async throws {
+        try await renderer.configure(camera: description.camera)
+    }
+    
+    func makeImage(for operation: ModelOperation) async throws -> CGImage {
+        try await renderer.perform(operation: operation)
+    }
+}
