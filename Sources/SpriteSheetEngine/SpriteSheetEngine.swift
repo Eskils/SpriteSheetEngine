6163012a398f4b479ca4b195857fb4392492931a
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-15 11:04:23 +0200

    Implement exporting sprite sheet to URL in SpriteSheetEngine

--- Sources/SpriteSheetEngine/SpriteSheetEngine.swift
+++ Sources/SpriteSheetEngine/SpriteSheetEngine.swift
@@ -1,5 +1,7 @@
 import Foundation
 import CoreGraphics
+import ImageIO
+import UniformTypeIdentifiers
 
 struct SpriteSheetEngine<Renderer: SpriteSheetRenderer> {
     let renderer: Renderer
@@ -23,8 +25,37 @@ struct SpriteSheetEngine<Renderer: SpriteSheetRenderer> {
     }
     
     func export(to url: URL) async throws {
+        switch description.export.kind {
+        case .image:
+            let image = try await spriteSheet()
+            let data = switch description.export.format {
+            case .png: try makeImageData(for: image, type: .png)
+            case .jpeg: try makeImageData(for: image, type: .jpeg)
+            }
+            try data.write(to: url)
+        }
+    }
+    
+    private func makeImageData(for image: CGImage, type: UTType) throws(ExportError)  -> Data {
+        let data = NSMutableData()
+        guard
+            let imageDestination = CGImageDestinationCreateWithData(data as CFMutableData, type.identifier as CFString, 1, nil)
+        else {
+            throw .cannotMakeImageDestination(type)
+        }
+        
+        CGImageDestinationAddImage(imageDestination, image, nil)
+        CGImageDestinationFinalize(imageDestination)
+        
+        return data as Data
     }
 }
 
 extension SpriteSheetEngine: Sendable {
+}
+
+extension SpriteSheetEngine {
+    enum ExportError: Error {
+        case cannotMakeImageDestination(UTType)
+    }
 }
