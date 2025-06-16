14fc6999e206f33b6b37f388b3dbc5e03c76c6fc
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-16 09:11:24 +0200

    Add syntax documentation to ModelSpriteSheetEngine

--- Sources/SpriteSheetEngine/ModelSpriteSheetEngine/ModelSpriteSheetEngine.swift
+++ Sources/SpriteSheetEngine/ModelSpriteSheetEngine/ModelSpriteSheetEngine.swift
@@ -8,10 +8,17 @@
 import Foundation
 import CoreGraphics
 
+/// Engine used to make sprite sheets from 3D-models.
+///
+/// You create this engine with a sprite sheet description ``SpriteSheetDescription.Model`` defining the model, camera and operations used to make the sprite sheet..
+/// This description can be made in code or parsed from a file, for instance json.
+/// For codable structure, see ``SpriteSheetDescription.ModelDTO``.
 public nonisolated struct ModelSpriteSheetEngine {
     let description: SpriteSheetDescription.Model
     let engine: SpriteSheetEngine<ModelSpriteSheetRenderer>
     
+    /// Construct new engine for making sprite sheets from 3D-models.
+    /// - Parameter description: Description defining the model, camera and operations used to make the sprite sheet.
     @MainActor
     public init(description: SpriteSheetDescription.Model) {
         self.description = description
@@ -19,16 +26,32 @@ public nonisolated struct ModelSpriteSheetEngine {
         self.engine = SpriteSheetEngine(renderer: renderer, description: description)
     }
     
+    /// Make sprite sheet as an image
+    /// - Returns: Sprite sheet as a CoreGraphics image
+    /// - Throws:
+    ///     - ``ImageTiler.ImageError``
+    ///     - ``RealityKitModelRendererError``
+    ///     - Possibly undocumented errors
     public func spriteSheet() async throws -> CGImage {
         try await engine.spriteSheet()
     }
     
+    /// Make sprite sheet as an image and save to disk
+    /// - Parameter url: File URL to the output path of the sprite sheet image
+    /// - Throws:
+    ///     - ``ImageTiler.ImageError``
+    ///     - ``RealityKitModelRendererError``
+    ///     - Possibly undocumented errors
     public func export(to url: URL) async throws {
         try await engine.export(to: url)
     }
 }
 
 extension ModelSpriteSheetEngine {
+    /// Construct new engine for making sprite sheets from 3D-models.
+    /// - Parameters:
+    ///   - data: Data of description defining the model, camera and operations used to make the sprite sheet.
+    ///   - type: The data format used to store the description
     @MainActor
     public init(decoding data: Data, type: SpriteSheetDescription.TransferableType) throws {
         switch type {
@@ -39,6 +62,10 @@ extension ModelSpriteSheetEngine {
         }
     }
     
+    /// Construct new engine for making sprite sheets from 3D-models.
+    /// - Parameters:
+    ///   - url: File URL to description defining the model, camera and operations used to make the sprite sheet.
+    ///   - type: The data format used to store the description
     @MainActor
     public init(url: URL, type: SpriteSheetDescription.TransferableType) throws {
         let data = try Data(contentsOf: url)
