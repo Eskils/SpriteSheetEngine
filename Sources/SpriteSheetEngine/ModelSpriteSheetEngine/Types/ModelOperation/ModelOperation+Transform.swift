8717e4ad92019afb45d07053434b01be5d5cc360
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-18 07:30:30 +0200

    Add syntax documentation to ModelOperation.Transform

--- Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/ModelOperation/ModelOperation+Transform.swift
+++ Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/ModelOperation/ModelOperation+Transform.swift
@@ -8,8 +8,11 @@
 import simd
 
 extension ModelOperation {
+    /// Structure that describes a transform operation
     public struct Transform: ModelApplicable {
+        /// A name that refers to a particular node or mesh
         public var nodeID: String
+        /// A matrix describing the transform to make on the node
         public var matrix: simd_float4x4
         
         public init(nodeID: String, matrix: simd_float4x4 = simd_float4x4(diagonal: .one)) {
