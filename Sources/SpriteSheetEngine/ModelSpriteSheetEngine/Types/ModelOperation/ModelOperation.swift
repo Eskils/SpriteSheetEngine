21ae12b18dd36b13c60b151fb6ce221d11a679e0
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-18 07:30:21 +0200

    Add syntax documentation to ModelOperation

--- Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/ModelOperation/ModelOperation.swift
+++ Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/ModelOperation/ModelOperation.swift
@@ -8,9 +8,14 @@
 import simd
 import CoreGraphics
 
+/// Possible operations for a node in a 3D scene
 public enum ModelOperation: SpriteSheetOperation {
+    /// Apply transform to a node
     case transform(Transform)
+    /// Change the color / material of  a node
     case material(Material)
+    /// Do nothing.
+    /// Can be used to produce a placeholder image to fill the sprite sheet grid.
     case none
 }
 
