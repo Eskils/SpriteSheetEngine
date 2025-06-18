2d4d158e64c5e1e1acd707b1af0f7e6c243cf81d
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-18 07:30:38 +0200

    Add syntax documentation to ModelOperation.Material

--- Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/ModelOperation/ModelOperation+Material.swift
+++ Sources/SpriteSheetEngine/ModelSpriteSheetEngine/Types/ModelOperation/ModelOperation+Material.swift
@@ -8,8 +8,13 @@
 import CoreGraphics
 
 extension ModelOperation {
+    /// Structure that describes a material change operation
     public struct Material: ModelApplicable {
+        /// A name that refers to a particular mesh.
+        /// If a node is found with this name, the operation will be applied to its first direct child mesh.
+        /// If the node has no mesh, an error will be thrown.
         public var nodeID: String
+        /// The diffuse color to be applied to the primary material.
         public var color: CGColor
         
         public init(nodeID: String, color: CGColor) {
