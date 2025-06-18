0452e27948c95fe9ddf233535c6a557242f792d7
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-18 19:17:39 +0200

    Add syntax documentation to SpriteSheetOperation

--- Sources/SpriteSheetEngine/Types/SpriteSheetOperation.swift
+++ Sources/SpriteSheetEngine/Types/SpriteSheetOperation.swift
@@ -5,5 +5,9 @@
 //  Created by Eskil Gjerde Sviggum on 04/06/2025.
 //
 
+/// Implement this interface to be recognized as a possible operation
+/// that can be used to produce tiles in a sprite sheet.
+///
+/// - SeeAlso: ``ModelOperation``
 public protocol SpriteSheetOperation {
 }
