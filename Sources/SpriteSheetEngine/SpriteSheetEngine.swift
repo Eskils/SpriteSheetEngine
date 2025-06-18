9d224246b2aa9f707e4cad7d7757ea6f614d51d4
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-18 19:40:47 +0200

    Add syntax documentation to SpriteSheetEngine

--- Sources/SpriteSheetEngine/SpriteSheetEngine.swift
+++ Sources/SpriteSheetEngine/SpriteSheetEngine.swift
@@ -3,10 +3,16 @@ import CoreGraphics
 import ImageIO
 import UniformTypeIdentifiers
 
+/// Engine used for making sprite sheets.
 public struct SpriteSheetEngine<Renderer: SpriteSheetRenderer> {
     let renderer: Renderer
     let description: Renderer.Description
     
+    /// Make a sprite sheet from the provided description using the given renderer.
+    /// - Returns: Tiled images as a single CoreGraphics image
+    /// - Throws:
+    ///     - ``ImageTiler.ImageError`` when there is a failure with making the image hosting all the tiles.
+    ///     - Renderer specific errors, when there is a failure with setting up the environment or making one of the tiles.
     public func spriteSheet() async throws -> CGImage {
         try await renderer.setup(description: description)
         
@@ -24,6 +30,18 @@ public struct SpriteSheetEngine<Renderer: SpriteSheetRenderer> {
         return try imageTiler.checkedFinish()
     }
     
+    /// Make a sprite sheet from the provided description using the given renderer and write to disk.
+    ///
+    /// The export type and image format can be set using the `export` property in the sprite sheet description.
+    /// For more information, see ``ExportSettings``
+    ///
+    /// - Parameter url: File URL pointing to where the produced image should be saved.
+    ///
+    /// - Throws:
+    ///     - ``ExportError`` when there is a failure encoding the image to the desired format
+    ///     - ``ImageTiler.ImageError`` when there is a failure with making the image hosting all the tiles.
+    ///     - Renderer specific errors, when there is a failure with setting up the environment or making one of the tiles.
+    ///     - Other errors, for example if a failure occurs when the image is being written to disk.
     public func export(to url: URL) async throws {
         switch description.export.kind {
         case .image:
@@ -55,7 +73,9 @@ extension SpriteSheetEngine: Sendable {
 }
 
 public extension SpriteSheetEngine {
+    /// Errors related to encoding images into image formats for saving to disk.
     enum ExportError: Error {
+        /// The image destination could not be made for the desired image format.
         case cannotMakeImageDestination(UTType)
     }
 }
