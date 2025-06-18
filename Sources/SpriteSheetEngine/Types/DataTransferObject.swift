1f967730d8be31ad0b569da94a84f8e3ed373d3f
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-18 19:21:51 +0200

    Add syntax documentation to DataTransferModelable, DataTransferTransferable and DataTransferObject

--- Sources/SpriteSheetEngine/Types/DataTransferObject.swift
+++ Sources/SpriteSheetEngine/Types/DataTransferObject.swift
@@ -5,16 +5,19 @@
 //  Created by Eskil Gjerde Sviggum on 08/06/2025.
 //
 
+/// Implement this interface as a data transfer object to be able to transform into a framework-native model/entity.
 protocol DataTransferModelable: Decodable {
     associatedtype Model
     associatedtype Failure: Error
     func toModel() throws(Failure) -> Model
 }
 
-protocol DataTransferTransferable: Decodable {
+/// Implement this interface as a data transfer object to be able to initialize from a framework-native model/entity.
+protocol DataTransferTransferable: Encodable {
     associatedtype Model
     init(model: Model)
 }
 
+/// Implement this interface as a data transfer object to be able to transform to and from a framework-native model/entity
 protocol DataTransferObject: DataTransferModelable, DataTransferTransferable {
 }
