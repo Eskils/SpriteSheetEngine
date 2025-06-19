4e308b5e5f7c16feff16fdd19e5860e475f84fc3
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-19 07:50:28 +0200

    Add syntax documentation to SpriteSheetDescribable.Model

--- Sources/SpriteSheetEngine/Types/SpriteSheetDescription/SpriteSheetDescription+Model.swift
+++ Sources/SpriteSheetEngine/Types/SpriteSheetDescription/SpriteSheetDescription+Model.swift
@@ -6,11 +6,19 @@
 //
 
 extension SpriteSheetDescription {
+    /// Description used for making a sprite sheet from a 3D-model
+    ///
+    /// - SeeAlso: ``SpriteSheetDescription.ModelDTO``
     public struct Model: SpriteSheetDescribable {
+        /// The kind of 3D Model to use for rendering
         public var model: ModelKind
+        /// Collection of properties that affect the camera in the scene.
         public var camera = CameraSettings()
+        /// Operations used to produce the tiles.
         public var operations = [ModelOperation]()
+        /// The number of tiles to place next to each other horizontally before expanding the sprite sheet verically.
         public var numberOfColumns = Int.max
+        /// Collection of properties used to describe how to export the sprite sheet.
         public var export = ExportSettings()
         
         public init(
