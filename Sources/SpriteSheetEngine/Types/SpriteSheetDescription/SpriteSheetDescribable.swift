d9d8dc0a02c97357672998201f11d2cde0e989ba
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-19 07:50:10 +0200

    Add syntax documentation to SpriteSheetDescribable

--- Sources/SpriteSheetEngine/Types/SpriteSheetDescription/SpriteSheetDescribable.swift
+++ Sources/SpriteSheetEngine/Types/SpriteSheetDescription/SpriteSheetDescribable.swift
@@ -5,9 +5,16 @@
 //  Created by Eskil Gjerde Sviggum on 04/06/2025.
 //
 
+/// Implement this interface to be recognized as a sprite sheet description.
+///
+/// - SeeAlso: ``SpriteSheetDescription.Model``
 public protocol SpriteSheetDescribable: Sendable {
     associatedtype Operation: SpriteSheetOperation
+    /// Operations used to produce the tiles.
+    /// Each operation corresponds to a tile.
     var operations: [Operation] { get }
+    /// The number of tiles to place next to each other horizontally before expanding the sprite sheet verically.
     var numberOfColumns: Int { get }
+    /// Collection of properties used to describe how to export the sprite sheet.
     var export: ExportSettings { get }
 }
