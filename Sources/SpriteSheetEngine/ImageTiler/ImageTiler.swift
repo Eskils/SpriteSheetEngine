faafac2dbe468dbbbab91b7011e9dc446e0dc5fb
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-19 08:06:37 +0200

    Add syntax documentation to ImageTiler

--- Sources/SpriteSheetEngine/ImageTiler/ImageTiler.swift
+++ Sources/SpriteSheetEngine/ImageTiler/ImageTiler.swift
@@ -7,6 +7,10 @@
 
 import CoreGraphics
 
+/// Engine used for tiling images as a grid into a larger image.
+///  
+/// Makes a full size image based on tile size, number of columns and number of images.
+/// Then each tile can be appended one by one.
 public nonisolated class ImageTiler {
     let context: CGContext
     let numberOfColumns: Int
@@ -15,6 +19,9 @@ public nonisolated class ImageTiler {
     let tileHeight: Int
     private var currentIndex = 0
     
+    /// Constructs a new image tiler by defining a grid from `tileSize`, `numberOfColumns` and `numberOfImages`.
+    /// - Throws:
+    ///     - ``ImageTiler.ImageError`` if the grid could not be made. For example if the grid would have invalid dimensions.
     public init(tileSize: CGSize, numberOfColumns: Int, numberOfImages: Int) throws(ImageError) {
         let actualNumberOfColumns = max(1, min(numberOfImages, numberOfColumns))
         let numberOfRows = Int(ceil(Float(numberOfImages) / Float(actualNumberOfColumns)))
@@ -40,6 +47,10 @@ public nonisolated class ImageTiler {
         self.numberOfImages = numberOfImages
     }
     
+    /// Arrange a subimage to the next available slot in the grid.
+    /// - Parameter tile: Subimage to arrange in the grid.
+    /// - Throws:
+    ///     - ``ImageTiler.TileError`` if the tile is too large or if the grid is full.
     public func checkedAppend(tile: CGImage) throws(TileError) {
         guard tile.width <= tileWidth && tile.height <= tileHeight else {
             throw .tileImageIsTooLarge(
@@ -57,6 +68,11 @@ public nonisolated class ImageTiler {
         append(tile: tile)
     }
     
+    /// Arrange a subimage to the next available slot in the grid.
+    ///
+    /// This method fails silently. For guarded arrangement, see ``checkedAppend(tile:)``.
+    ///
+    /// - Parameter tile: Subimage to arrange in the grid.
     public func append(tile: CGImage) {
         let column = currentIndex % numberOfColumns
         let row = currentIndex / numberOfColumns
@@ -74,10 +90,23 @@ public nonisolated class ImageTiler {
         currentIndex += 1
     }
     
+    /// Produce an image from the grid.
+    ///
+    /// It is safe to call this method multiple times.
+    /// This method returns nil upon failure. For guarded finish, see ``checkedFinish()``.
+    ///
+    /// - Returns: Image of tiled grid as a CoreGraphics image
     public func finish() -> CGImage? {
         context.makeImage()
     }
     
+    /// Produce an image from the grid.
+    ///
+    /// It is safe to call this method multiple times.
+    ///
+    /// - Returns: Image of tiled grid as a CoreGraphics image
+    /// - Throws:
+    ///     - ``ImageTiler.ImageError`` if the final image could not be made.
     public func checkedFinish() throws(ImageError) -> CGImage {
         guard let image = finish() else {
             throw .cannotMakeImage
