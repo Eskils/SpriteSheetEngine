35e59ad612a181358bb17178a2bfbfb19d85b326
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-19 08:06:55 +0200

    Add syntax documentation to ImageTiler errors

--- Sources/SpriteSheetEngine/ImageTiler/ImageTiler+ImageError.swift
+++ Sources/SpriteSheetEngine/ImageTiler/ImageTiler+ImageError.swift
@@ -6,8 +6,12 @@
 //
 
 extension ImageTiler {
+    /// Errors related to making the grid image
     public enum ImageError: Error {
+        /// Could not make a Core Graphics context with the given configuration.
+        /// Possibly due to invalid dimensions.
         case cannotMakeCGContext(width: Int, height: Int)
+        /// The final image could not be made.
         case cannotMakeImage
     }
 }
--- Sources/SpriteSheetEngine/ImageTiler/ImageTiler+TileError.swift
+++ Sources/SpriteSheetEngine/ImageTiler/ImageTiler+TileError.swift
@@ -6,8 +6,11 @@
 //
 
 extension ImageTiler {
+    /// Errors related to arranging tiles
     public enum TileError: Error {
+        /// The tile cannot be arranged as its size exceeds the allocated bounds.
         case tileImageIsTooLarge(actualWidth: Int, actualHeight: Int, expectedWidth: Int, expectedHeight: Int)
+        /// The tile cannot be arranged as the grid is full.
         case tileIndexOutOfBounds(index: Int, count: Int)
     }
 }
