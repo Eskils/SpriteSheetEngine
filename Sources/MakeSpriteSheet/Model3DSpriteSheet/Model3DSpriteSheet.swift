c6299927d037f7101ffeb003bfa5a39cd3d8c859
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-20 08:57:56 +0200

    Implement support for relative paths in Model3DSpriteSheet

--- Sources/MakeSpriteSheet/Model3DSpriteSheet/Model3DSpriteSheet.swift
+++ Sources/MakeSpriteSheet/Model3DSpriteSheet/Model3DSpriteSheet.swift
@@ -9,7 +9,7 @@ import Foundation
 import ArgumentParser
 import SpriteSheetEngine
 
-struct Model3DSpriteSheet: ParsableCommand {
+struct Model3DSpriteSheet: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "model",
         abstract: "Make sprite sheet from a 3D model."
@@ -30,33 +30,46 @@ struct Model3DSpriteSheet: ParsableCommand {
         ),
     )
     var output: String
-    
-    func validate() throws(ValidationError) {
-        guard FileManager.default.fileExists(atPath: input) else {
-            throw .invalidInputPath(input)
-        }
-        
-        guard FileManager.default.isWritableFile(atPath: output) else {
-            throw .invalidOutputPath(output)
-        }
-    }
 
     mutating func run() async throws {
-        let inputURL = if #available(macOS 13.0, *) {
-            URL(filePath: input)
+        let currentWorkingDirectory = if #available(macOS 13.0, *) {
+            #if DEBUG
+            if ProcessInfo.processInfo.environment["XCODE_RUN"] != nil {
+                URL(filePath: #filePath + "../../../../../").standardizedFileURL
+            } else {
+                URL(filePath: FileManager.default.currentDirectoryPath)
+            }
+            #else
+            URL(filePath: FileManager.default.currentDirectoryPath)
+            #endif
         } else {
-            URL(fileURLWithPath: input)
+            URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+        }
+        
+        let inputURL = if #available(macOS 13.0, *) {
+            URL(filePath: input, relativeTo: currentWorkingDirectory)
+        } else {
+            URL(fileURLWithPath: input, relativeTo: currentWorkingDirectory)
         }
         
         let outputURL = if #available(macOS 13.0, *) {
-            URL(filePath: output)
+            URL(filePath: output, relativeTo: currentWorkingDirectory)
         } else {
-            URL(fileURLWithPath: output)
+            URL(fileURLWithPath: output, relativeTo: currentWorkingDirectory)
+        }
+        
+        guard FileManager.default.fileExists(atPath: inputURL.path) else {
+            throw ValidationError.invalidInputPath(input)
+        }
+        
+        guard FileManager.default.isWritableFile(atPath: outputURL.deletingLastPathComponent().path) else {
+            throw ValidationError.invalidOutputPath(output)
         }
         
         let engine = try await ModelSpriteSheetEngine(
             url: inputURL,
-            type: .json
+            type: .json,
+            relativeTo: inputURL.deletingLastPathComponent()
         )
         
         try await engine.export(to: outputURL)
