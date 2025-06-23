b39a8218ab3f6b4e4e9f3021a24df714a4dc20e5
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-06-23 17:28:23 +0200

    Add tests for hsb decode failures

--- Tests/SpriteSheetEngineTests/CodableTests/ColorDTOTests.swift
+++ Tests/SpriteSheetEngineTests/CodableTests/ColorDTOTests.swift
@@ -72,6 +72,92 @@ struct ColorDTOTests {
     }
     
     @Test
+    func decodeHSBWithoutSpaces() throws {
+        let string = "\"hsb(0.1,0.2,0.3)\""
+        let encoded = string.data(using: .utf8)!
+        let dto = try decoder.decode(ColorDTO.self, from: encoded)
+        #expect(dto == .hsb(0.1, 0.2, 0.3))
+    }
+    
+    @Test
+    func decodeHSBFailsWithTooFewArguments() throws {
+        let string = "\"hsb(0.1, 0.2)\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: ColorDTO.DecoderError.tooFewComponentsForHSB(count: 2, expected: 3)) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+    }
+    
+    @Test
+    func decodeHSBSucceedsWithTooManyArguments() throws {
+        let string = "\"hsb(0.1, 0.2, 0.3, 0.4)\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: Never.self) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+    }
+    
+    @Test
+    func decodeHSBFailsWithIncorrectFormat() throws {
+        let string = "\"hsb(0.1, 0.2, )\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: ColorDTO.DecoderError.cannotConvertStringToFloat("")) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+    }
+    
+    @Test
+    func decodeHSBFailsWithInvalidNumber() throws {
+        let string = "\"hsb(0.1, 0.2, fh)\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: ColorDTO.DecoderError.cannotConvertStringToFloat("fh")) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+    }
+    
+    @Test
+    func decodeHSBFailsWithOutOfBoundsForAbove1() throws {
+        let string = "\"hsb(0.1, 0.2, 1.2)\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: ColorDTO.DecoderError.numberExceedsAllowedBounds(value: 1.2, min: 0, max: 1)) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+    }
+    
+    @Test
+    func decodeHSBFailsWithOutOfBoundsForBelow0() throws {
+        let string = "\"hsb(0.1, 0.2, -0.3)\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: ColorDTO.DecoderError.numberExceedsAllowedBounds(value: -0.3, min: 0, max: 1)) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+    }
+    
+    @Test
+    func decodeHSBSucceedsWithValueExactly1() throws {
+        let string = "\"hsb(0.1, 0.2, 1)\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: Never.self) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+    }
+    
+    @Test
+    func decodeHSBSucceedsWithValueExactly0() throws {
+        let string = "\"hsb(0.1, 0.2, 0)\""
+        let encoded = string.data(using: .utf8)!
+        #expect(throws: Never.self) {
+            try decoder.decode(ColorDTO.self, from: encoded)
+        }
+        
+        let string2 = "\"hsb(0.1, 0.2, -0)\""
+        let encoded2 = string2.data(using: .utf8)!
+        #expect(throws: Never.self) {
+            try decoder.decode(ColorDTO.self, from: encoded2)
+        }
+    }
+    
+    @Test
     func hsbToModel() throws {
         let dto = ColorDTO.hsb(0.1, 0.2, 0.3)
         let model = dto.toModel()
